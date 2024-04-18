6de1ae7132974ca2a876ddc19845ca45ed1b31ff
Gabriel Acosta <gabriacos@gmail.com>
2024-04-18 11:34:49 -0300

    feat: Se agregó la opción de escuchar de varios puertos

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from controllers.consultas_controller import consultas
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
 app = FastAPI()
 
@@ -18,6 +19,9 @@ class DatosConsulta(BaseModel):
 
 @app.post("/consultas/")
 async def procesar_consulta(datos: DatosConsulta):
+    print("datos: ", datos)
     return consultas(datos)
 
-
+if __name__ == "__main__":
+    # Aquí se especifica la dirección IP y el puerto en el que el servidor escuchará
+    uvicorn.run(app, host="0.0.0.0", port=8000)
